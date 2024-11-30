85b90eb7f6f370f8bc6b5ffc818e42e74aa73e67
mubashir1osmani <ilikewafflesomcuh@gmail.com>
2024-11-29 23:27:25 -0500

    about

---
+++ app/about/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+export default function About() {
+    return (
+        <div className="md:flex hidden">
+            <h1 className="mb-4 text-4xl">
+                About Us
+            </h1>
+            <p></p>
+        </div>
+    )
+
+}
