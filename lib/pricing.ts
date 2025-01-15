8216718e1b1fdc2f517576f78308311ba65b669f
mubashir1osmani <mubashir.osmani777@gmail.com>
2025-01-15 01:21:22 -0500

    lib functions

---
+++ lib/pricing.ts
@@ -0,0 +1,18 @@
+import { User } from "@supabase/supabase-js";
+
+export interface PricingTier {
+    title: string;
+    price: string;
+    description: string;
+    features: string[];
+    buttonText?: string;
+    priceId?: string;
+    user: User | null;
+    index: number;
+}
+
+export interface PricingPageProps {
+    user: User | null;
+}
+
+export type PricingTierData = Omit<PricingTier, "user">;    
